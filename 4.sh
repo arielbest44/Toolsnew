5a19009cd48dbff20b2698c103407fa7c0a49f5e
arielbest44 <56075835+arielbest44@users.noreply.github.com>
2019-10-10 14:31:41 +0700

    Add files via upload

---
+++ 4.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+#version 50.0
+#Mao Ngapain?
+#Mao Recode?
+#Utamakan Izin Dulu^_^
+
+clear
+#variabels
+b='\033[34;1m'
+g='\033[32;1m'
+p='\033[35;1m'
+c='\033[36;1m'
+r='\033[31;1m'
+w='\033[37;1m'
+y='\033[33;1m'
+python wk.py
+
+sleep 1
+clear
+sleep 1
+echo $c"♥ LOADING ♥"
+sleep 3
+clear
+toilet -f future "love you" | lolcat
+sleep 5
+clear
+toilet -f standard " Apriyani " -F gay
+sleep 2
+clear
+figlet Fb Tools | lolcat
+
+echo $y"∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
+
+                 [ TOOLS Facebook ]
+
+♦ Coded   : SQU4D
+♦ Team    : Termux1928 Cyber
+♦ Github  : https://github.com/SQU4D1928
+♦ Youtube : AG Official
+
+∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo -b $g"[#]> (Ctrl + C ) Detected, Trying To Exit ...."
+echo -b $g"[#]> GUNAKAN TOOLS INI DENGAN BIJAK BOOSSKU "
+sleep 1
+echo ""
+echo -b $g"[#]> BLANK"
+
+echo -b $g"[#]> See you Again :)..."
+sleep 1
+exit
+}
+
+lagi=1
+while [ $lagi -lt 6 ];
+do
+echo""
+echo""
+echo""
+echo $c"1. BruteForce Facebook MBF "
+echo $c"∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
+echo $r"2. BruteForce Facebook  "
+echo $r"∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
+echo $g"3. Cloning Yahoo "
+echo $g"∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
+echo $y"4. Bot Komen Facebook "
+echo $y"∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
+echo $b"5. Auto Report Facebook "
+echo $b"∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
+echo $g"6. Profile Guard Facebook "
+echo $g"∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
+echo $r"7. Dark Fb Versi 2.1"
+echo $g"∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
+echo $b"0. Keluar"
+echo $g
+read -p " Pilih No : " pil;
+
+case $pil in
+1) clear
+toilet -f standard " SQU4D " -F gay
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+
+;;
+
+2) clear
+toilet -f standard " SQU4D " -F gay
+git clone https://github.com/sixtysix-Team/fbbrute
+cd fbbrute
+python2 force.py
+
+;;
+
+3) clear
+toilet -f standard " SQU4D " -F gay
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+
+;;
+
+4) clear
+toilet -f standard " SQU4D " -F gay
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+
+;;
+
+5) clear
+toilet -f standard " SQU4D " -F gay
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+
+;;
+
+6) clear
+toilet -f standard " APRIYANI " -F gay
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+
+;;
+
+7) clear
+toilet -f standard " Dark FB " -F gay
+git clone https://github.com/arielbest44/1.9
+cd 1.9
+python2 xz.py
+
+;;
+
+00) toilet -f standard " APRIYANI " -F gay
+echo $g"Keluar....."
+exit
+;;
+
+esac
+done
+done
+
